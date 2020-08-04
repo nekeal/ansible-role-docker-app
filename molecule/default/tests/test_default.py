a11969f832de7d3f2d6b78129f4a2e4de5fa5306
Szymon Cader <szymon.sc.cader@gmail.com>
2020-08-04 17:55:47 +0200

    Add support for one time pre_release commands

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,3 +5,8 @@ import requests
 def test_nginx_is_accessible(host):
     r = requests.get('http://localhost/')
     r.raise_for_status()
+
+
+def test_file_is_created_during_pre_release_command(host):
+    file = host.file("/tmp/pre_release_part")
+    assert file.exists
