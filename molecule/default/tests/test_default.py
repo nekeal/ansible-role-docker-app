ed6ae1dcd30aa3a00bf2923541525921a5c05490
Szymon Cader <szymon.sc.cader@gmail.com>
2020-08-05 21:51:18 +0200

    Add additional image and container for test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -21,3 +21,9 @@ def test_post_release_is_executed_after_pre_release_command(host):
     pre_release_file = host.file("/tmp/pre_release_part")
     post_release_file = host.file("/tmp/post_release_part")
     assert pre_release_file.mtime < post_release_file.mtime
+
+
+def test_django_is_accessible(host):
+    r = requests.get("http://localhost:8000")
+    r.raise_for_status()
+    assert b"The install worked successfully! Congratulations!" in r.content
